58a604e6497eb1e2c48b2f4d2a5baad467c177bd
tanguy chennaux <tanguy.chennaux@gmail.com>
2020-11-16 18:43:16 +0000

    sending emails if it become available

--- script.py
+++ script.py
@@ -1,4 +1,28 @@
-import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from ps5Email import send_email_alert
 
-r = requests.get("https://www.johnlewis.com")
-print(r.status_code)
+# basic variables
+url = "https://www.johnlewis.com/sony-playstation-5-console-with-dualsense-controller/white/p5115192"
+path = "./chromedriver"
+
+# set the browser
+driver = webdriver.Chrome(path)
+# make a call using the driver
+response = driver.get(url)
+
+# to remove the cookie spam
+search = driver.find_element_by_xpath(
+    '//*[@id="pecr-cookie-banner-wrapper"]/div/div[1]/div/div[2]/button[1]'
+)
+search.send_keys(Keys.RETURN)
+
+# look for add to basket
+available = driver.find_elements_by_id("button--add-to-basket")
+if available:
+    # add email sending method
+    print("email me")
+    send_email_alert()
+else:
+    print("still unavailable")
+# browser.close()
