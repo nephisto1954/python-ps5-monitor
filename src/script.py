73c56304323f61f54fca4ab45a91121b0a6a10d5
tanguy chennaux <tanguy.chennaux@gmail.com>
2020-11-17 17:14:15 +0000

    small changes webdriver

--- src/script.py
+++ src/script.py
@@ -2,20 +2,21 @@ import os
 import schedule
 import time
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from ps5Email import send_email_alert
 
 
 def job():
 
-    op = webdriver.ChromeOptions()
+    op = Options()
     op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     op.add_argument("--headless")
     op.add_argument("--no-sandbox")
     op.add_argument("--disable-dev-sh-usage")
 
     driver = webdriver.Chrome(
-        executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op
+        executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=op
     )
 
     driver.get("https://www.youtube.com")
