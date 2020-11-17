7ce736a67cff4928d26ccf02d6aeb26c9b06faa2
tanguy chennaux <tanguy.chennaux@gmail.com>
2020-11-17 12:29:29 +0000

    make it better

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+import os
 import schedule
 import time
 from selenium import webdriver
@@ -6,35 +7,49 @@ from ps5Email import send_email_alert
 
 
 def job():
-    # basic variables
-    url = "https://www.johnlewis.com/sony-playstation-5-console-with-dualsense-controller/white/p5115192"
-    path = "./chromedriver"
 
-    # set the browser
-    driver = webdriver.Chrome(path)
-    # make a call using the driver
-    driver.get(url)
+    op = webdriver.ChromeOptions()
+    op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    op.add_argument("--headless")
+    op.add_argument("--no-sandbox")
+    op.add_argument("--disable-dev-sh-usage")
 
-    available = driver.find_elements_by_id("button--add-to-basket")
+    driver = webdriver.Chrome(
+        executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op
+    )
 
-    if available:
-        # add email sending method
-        print("email me")
-        send_email_alert()
-    else:
-        print("still unavailable")
+    driver.get("https://www.youtube.com")
+    print(driver.page_source)
 
-    # look for add to basket
-    while not available:
-        Keys.TAB
-        Keys.RETURN
+    # # basic variables
+    # url = "https://www.johnlewis.com/sony-playstation-5-console-with-dualsense-controller/white/p5115192"
+    # path = "./chromedriver"
 
-        time.sleep(20)
-        driver.quit()
-        # # to remove the cookie spam
-        # for x in xPaths:
-        #     search = driver.find_element_by_xpath(x)
-        #     search.send_keys(Keys.RETURN)
+    # # set the browser
+    # driver = webdriver.Chrome(path)
+    # # make a call using the driver
+    # driver.get(url)
+
+    # available = driver.find_elements_by_id("button--add-to-basket")
+
+    # if available:
+    #     # add email sending method
+    #     print("email me")
+    #     send_email_alert()
+    # else:
+    #     print("still unavailable")
+
+    # # look for add to basket
+    # while not available:
+    #     Keys.TAB
+    #     Keys.RETURN
+
+    #     time.sleep(20)
+    #     driver.quit()
+    #     # # to remove the cookie spam
+    #     # for x in xPaths:
+    #     #     search = driver.find_element_by_xpath(x)
+    #     #     search.send_keys(Keys.RETURN)
 
 
 # scheduling process/setup
